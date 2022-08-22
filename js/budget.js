1ea61a4ae08a5803003462885ef7e4b803c3ea1c
SamaulHaque <samaulhaque749@gmail.com>
2022-08-22 20:44:51 +0600

    Header section added

---
+++ js/budget.js
@@ -0,0 +1,50 @@
+
+/*--------------------------------------------------------------
+                    Budget section start here
+----------------------------------------------------------------*/
+//function for calculte event handler
+function amountFieldValue(fieldID){
+    const getFieldValue=document.getElementById(fieldID);
+    const getFieldValueString=getFieldValue.value;
+    const fieldValue=parseFloat(getFieldValueString);
+    getFieldValue.value=fieldValue;
+    getFieldValue.value='';
+    return fieldValue;
+}
+
+//calculat event handler
+document.getElementById('calculate-btn').addEventListener('click', function(){
+    const perPlayerAmount=amountFieldValue('per-player-amonut');
+    if(isNaN(perPlayerAmount)){
+        alert("Input Field Can't Be Empty. \nPlease Enter a Valid Number.");
+        return;
+    }
+    const playerExpenses=document.getElementById('player-expenses');
+
+    let list = document.querySelector("#select-five"),
+    listLi = list.getElementsByTagName("li"),
+    listLength = listLi.length;
+
+    const playerExpensesTotal=perPlayerAmount*listLength;
+    playerExpenses.innerText=playerExpensesTotal;
+})
+
+//calculate total event handler
+document.getElementById('calculate-total-btn').addEventListener('click', function(){
+    const managerAmount=amountFieldValue('manager-amonut');
+    const coachAmount=amountFieldValue('coach-amount');
+    if(isNaN(managerAmount && coachAmount)){
+        alert("Input Field Can't Be Empty. \nPlease Enter a Valid Number");
+        return;
+    }
+
+    const playerExpensesAmonut=document.getElementById('player-expenses');
+    const playerExpensesAmonutString=playerExpensesAmonut.innerText;
+    const expensesAmount=parseFloat(playerExpensesAmonutString);
+    playerExpensesAmonut.innerText=expensesAmount;
+
+    const totalExpenses=expensesAmount+managerAmount+coachAmount;
+
+    const totalField=document.getElementById('total-amount');
+    totalField.innerText=totalExpenses;
+})
---
+++ js/selectFive.js
@@ -0,0 +1,68 @@
+/*--------------------------------------------------------------
+                    Fucntion for select 5 player start here
+----------------------------------------------------------------*/
+function getPlayerTitle(nameId){
+    const getplayerName=document.getElementById(nameId);
+    const playerName=getplayerName.innerText;
+    return playerName;
+}
+
+function list(playerID){
+    let list = document.querySelector("#select-five"),
+    listLi = list.getElementsByTagName("li"),
+    listLength = listLi.length;
+    if(listLength==5){
+      alert("You Can't Select More Than Five Players. \nYou Already Select Five Players");
+      return ;
+    } 
+    const SelectPlayerName=getPlayerTitle(playerID)
+    const listContainer=document.getElementById('select-five');
+    const orderList=document.createElement('li');
+    orderList.innerText=SelectPlayerName;
+    listContainer.appendChild(orderList);
+}
+/*--------------------------------------------------------------
+            Select 5 Player Button Event Handler start here
+----------------------------------------------------------------*/
+// messi button event handler
+document.getElementById('messi-btn').addEventListener('click',function(){
+  list('lionel-massi');
+  document.getElementById("messi-btn").style.backgroundColor='gray';
+  document.getElementById("messi-btn").disabled = true;
+})
+
+// ronaldo button event handler
+document.getElementById('ronaldo-btn').addEventListener('click',function(){
+  list('ronaldo');
+  document.getElementById("ronaldo-btn").style.backgroundColor='gray';
+  document.getElementById("ronaldo-btn").disabled = true;
+})
+
+// di maria button event handler
+document.getElementById('di-maria-btn').addEventListener('click',function(){
+  list('di-maria');
+  document.getElementById("di-maria-btn").style.backgroundColor='gray';
+  document.getElementById("di-maria-btn").disabled = true;
+})
+
+// dybala button event handler
+document.getElementById('dybala-btn').addEventListener('click',function(){
+  list('dybala');
+  document.getElementById("dybala-btn").style.backgroundColor='gray';
+  document.getElementById("dybala-btn").disabled = true;
+})
+
+// pique button event handler
+document.getElementById('pique-btn').addEventListener('click',function(){
+  list('pique');
+  document.getElementById("pique-btn").style.backgroundColor='gray';;
+  document.getElementById("pique-btn").disabled = true;
+})
+
+// salah button event handler
+document.getElementById('salah-btn').addEventListener('click',function(){
+  list('salah');
+  document.getElementById("salah-btn").style.backgroundColor='gray';
+  document.getElementById("salah-btn").disabled = true;
+
+})
